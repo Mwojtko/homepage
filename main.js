5deade24a719475848b6bf4e62840fa637c056a2
Mikołaj Wojtkowiak <mikolaj.wojtkowiak@tlen.pl>
2019-06-10 19:48:19 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('Siema!');
+const name='Mikołaj';
+const age=26;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
---
+++ main2.js
@@ -0,0 +1,5 @@
+console.log(`Siema!`);
+console.log(`Witam Cię na mojej stronie!`)
+const name=`Mikołaj Wojtkowiak`;
+const age=26;
+console.log(`Pozdrawiam serdecznie ${name} w wieku ${age}`);
