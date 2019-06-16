8fd536bc0ae855ae2d15e198b74f47ad4e05e29d
Mikołaj Wojtkowiak <mikolaj.wojtkowiak@tlen.pl>
2019-06-16 18:39:25 +0200

    feat: new classes for paragraphs

--- main2.js
+++ main2.js
@@ -3,3 +3,15 @@ console.log(`Witam Cię na mojej stronie!`)
 const name=`Mikołaj Wojtkowiak`;
 const age=26;
 console.log(`Pozdrawiam serdecznie ${name} w wieku ${age}`);
+
+const about = document.querySelector(`.about__paragraph--js`);
+console.log(about.innerHTML);
+
+about.innerHTML = ('Pozdro JS');
+
+const paragraphs = document.querySelectorAll(`p`);
+console.log(paragraphs);
+
+console.log(paragraphs[3]);
+
+
