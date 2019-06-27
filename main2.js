094932492851195ec25f8e94e17ecd211c002f58
Mikołaj Wojtkowiak <mikolaj.wojtkowiak@tlen.pl>
2019-06-27 18:18:25 +0200

    Feat: Button on main page which changes the main header

--- main2.js
+++ main2.js
@@ -1,8 +1,8 @@
 console.log(`Siema!`);
 console.log(`Witam Cię na mojej stronie!`)
-const name=`Mikołaj Wojtkowiak`;
-const age=26;
-console.log(`Pozdrawiam serdecznie ${name} w wieku ${age}`);
+//const name=`Mikołaj Wojtkowiak`;
+//const age=26;
+//console.log(`Pozdrawiam serdecznie ${name} w wieku ${age}`);
 
 const about = document.querySelector(`.about__paragraph--js`);
 if (about) 
@@ -31,8 +31,34 @@ function welcome (age, name) {
     }
 }
 
+const welcome2 = (name, age) => {
+    console.log(`Witaj ${name} widzę że masz ${age} lat!`)
+}
+
+welcome2 ('Mikołaj', 26);
+
+const myName = 'Mikołaj';
+const myAge = 26;
+
+welcome2(myName,myAge);
 
 
+//Funkcje dotyczące przycisku na stronie:
+
+const button = document.querySelector('.header__button--js'); //Tutaj powiązano przycisk utworzony z HTML do JS
+
+//function handleClick() {}   // Tutaj zdefiniowano funkcje do klikniecia
+
+
+//button.addEventListener('click', handleClick); //Tutaj dodano funkcję, do obslugi klikniecia 
+                                                //Dodatkowo, tutaj jest zdefiniowany addEventListener jako type 'clik' oraz podanie nazwy funkcji
+
+//Tu już właściwa funkcja wykonująca funkcję zamiany tekstu poprzez kliknięcie
+
+button.addEventListener('click', (e) => {                   //"Nasłuchuje" co ma się zmienic". Użycie funkcji Fat Arrow
+    const header = document.querySelector('.header--js');     //Tutaj jest odwołanie do zmienianego nagłówka
+    header.innerHTML = 'klik, klik!';          //Tutaj podmienia tekst
+});
 
 
 
